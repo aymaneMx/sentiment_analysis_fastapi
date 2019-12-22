638658fbf9f245353d1e1a0b37414aab273c5eaf
aymaneMx <aymanemx@gmail.com>
2019-12-22 18:27:42 +0100

    add analysis endpoint

--- sentiment_analysis_fastapi/main.py
+++ sentiment_analysis_fastapi/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from textblob import TextBlob
+
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"msg": "Alive!"}
+
+
+@app.get("/analysis/")
+def analysis_result(q: str = None):
+    if q:
+        polarity = TextBlob(q).sentences[0].polarity
+        return {"sentence": q, "polarity": polarity}
+    else:
+        return {"Error": "plz enter a sentence!"}
